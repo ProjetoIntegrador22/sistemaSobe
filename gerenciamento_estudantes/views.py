6403b6af2fef882802d0183f0987dbfa6ba91ebd
Diogo <digo.pasi@gmail.com>
2024-11-13 14:22:35 -0300

    All Side Menu

--- gerenciamento_estudantes/views.py
+++ gerenciamento_estudantes/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from gerenciamento_estudantes.models import AdminEscola
 
 def redirect_to_login(request):
     return redirect('/accounts/login')
 
-def profile(request):
-    return render(request, 'profile.html')
+def profile(request, ):
+    if request.user.is_superuser:
+        context = {'usuario': 'superuser', 'nome': 'Diogo'}
+    elif request.user.groups.filter(name='Administracao Escola').exists():
+        # Usuário é um admin
+        usuario_atual = AdminEscola.objects.get(usuario=request.user)
+        context = {'profile': usuario_atual}
+    elif request.user.groups.filter(name='Professores').exists():
+        # Usuário é um professor
+        context = {'usuario': 'professor'}
+    else:
+        # Usuário é um estudante
+        context = {'usuario': 'aluno'}
+    return render(request, 'profile.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
@@ -14,10 +27,15 @@ def sobre(request):
     return render(request, 'sobre.html')
 
 def test(request):
-    if request.user.groups.filter(name='admin').exists():
-        # User is an admin
-        context = {'is_admin': True}
+    if request.user.is_superuser:
+        context = {'usuario': 'superuser'}
+    elif request.user.groups.filter(name='Administracao Escola').exists():
+        # Usuário é um admin
+        context = {'usuario': 'admin'}
+    elif request.user.groups.filter(name='Professores').exists():
+        # Usuário é um professor
+        context = {'usuario': 'professor'}
     else:
-        # User is not an admin
-        context = {'is_admin': False}
-    return render(request, 'my_template.html', context)
+        # Usuário é um estudante
+        context = {'usuario': 'aluno'}
+    return render(request, 'test.html', context)
